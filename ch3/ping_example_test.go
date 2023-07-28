3f9a4793f41e02924cc9f40e9280e6a0b3973626
rutaceae <1593713783@qq.com>
2023-07-28 09:21:54 +0900

    networkProgram day4

--- ch3/ping_example_test.go
+++ ch3/ping_example_test.go
@@ -4,24 +4,17 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"testing"
 	"time"
 )
 
-func TestPingExample(t *testing.T) {
+func ExamplePinger() {
 
-	// 创建一个可以被取消的context对象，cancel函数被用来在适当的时候结束pinger goroutine
 	ctx, cancel := context.WithCancel(context.Background())
-	// 使用io.Pipe 创建一对读写管道
 	r, w := io.Pipe()
-	// 用于在Pinger goroutine结束时发送信号。
 	done := make(chan struct{})
-	// 创建了一个resetTimer通道，它是一个带缓冲的通道，用于发送新的ping间隔到Pinger函数。
 	resetTimer := make(chan time.Duration, 1)
-	// 向resetTimer通道发送了一个时间间隔，这是首次ping的间隔
 	resetTimer <- time.Second // initial ping interval
 
-	// 在新的goroutine中启动Ping函数，并在函数结束时关闭done 通道
 	go func() {
 		Pinger(ctx, w, resetTimer)
 		close(done)
@@ -29,23 +22,23 @@ func TestPingExample(t *testing.T) {
 
 	receivePing := func(d time.Duration, r io.Reader) {
 		if d >= 0 {
-			fmt.Printf("resetting timer (%s)\n", d)
+			fmt.Printf("resetting time (%s)\n", d)
 			resetTimer <- d
 		}
+		now := time.Now()
+		buf := make([]byte, 1024)
+		n, err := r.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Printf("received %q (%s)\n",
+			buf[:n], time.Since(now).Round(100*time.Millisecond))
 	}
-	now := time.Now()
-	buf := make([]byte, 1024)
-	n, err := r.Read(buf)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("received %q (%s)\n", buf[:n],
-		time.Since(now).Round(100*time.Millisecond))
 	for i, v := range []int64{0, 200, 300, 0, -1, -1, -1} {
-		fmt.Printf("Run %d\n", i+1)
+		fmt.Printf("Run %d:\n", i+1)
 		receivePing(time.Duration(v)*time.Millisecond, r)
 	}
 	cancel()
-	<-done //ensures the pinger exits after canceling the context
-
+	<-done
 }
