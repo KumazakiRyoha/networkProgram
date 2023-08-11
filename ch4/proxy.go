d42b7fabca0eec3a9de5d102749d0dd52fef2422
rutaceae <1593713783@qq.com>
2023-08-11 23:56:21 +0900

    networkProgram day9

---
+++ ch4/proxy.go
@@ -0,0 +1,16 @@
+package main
+
+import "io"
+
+func proxy(from io.Reader, to io.Writer) error {
+	fromWriter, fromIsWriter := from.(io.Writer)
+	toReader, toIsReader := to.(io.Reader)
+	if toIsReader && fromIsWriter {
+		// Send replies since "from" and "to" implement the necessary interface
+		go func() {
+			_, _ = io.Copy(fromWriter, toReader)
+		}()
+	}
+	_, err := io.Copy(to, from)
+	return err
+}
