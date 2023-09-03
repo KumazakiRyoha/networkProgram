9893c9aa792b1f18ebfc9bb6204606057a3bcaf8
rutaceae <1593713783@qq.com>
2023-09-03 14:55:16 +0900

    networkProgram day15

---
+++ ch6/Server.go
@@ -0,0 +1,128 @@
+package tftp
+
+import (
+	"bytes"
+	"errors"
+	"log"
+	"net"
+	"time"
+)
+
+type Server struct {
+	Payload []byte        // the payload served for all read requests
+	Retries uint8         // the number of times to retry a failed transmission
+	Timeout time.Duration // the duration to wait for an acknowledgment
+}
+
+func (s Server) ListenAndServer(addr string) error {
+	conn, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = conn.Close() }()
+	log.Printf("Listening on %s ...\n", conn.LocalAddr())
+
+	return s.Server(conn)
+}
+
+func (s *Server) Server(conn net.PacketConn) error {
+	if conn == nil {
+		return errors.New("nil connection")
+	}
+
+	if s.Payload == nil {
+		return errors.New("payload is required")
+	}
+	if s.Retries == 0 {
+		s.Retries = 10
+	}
+
+	if s.Timeout == 0 {
+		s.Timeout = 5 * time.Second
+	}
+
+	var rrq ReadReq
+	for {
+		buf := make([]byte, DatagramSize)
+		// conn.ReadFrom(buf)是阻塞性的 它会等待直到有数据包到达。
+		// 如果没有数据到达，这个调用会一直阻塞
+		_, addr, err := conn.ReadFrom(buf)
+		if err != nil {
+			return err
+		}
+
+		err = rrq.UnmarshalBinary(buf)
+		if err != nil {
+			log.Printf("[%s] bad request: %v", addr, err)
+			continue
+		}
+
+		go s.handle(addr.String(), rrq)
+	}
+
+}
+
+func (s Server) handle(clientAddr string, rrq ReadReq) {
+	log.Printf("[%s] request file: %s", clientAddr, rrq.Filename)
+
+	conn, err := net.Dial("udp", clientAddr)
+	if err != nil {
+		log.Printf("[%s] dial: %v", clientAddr, err)
+		return
+	}
+	defer func() { _ = conn.Close() }()
+
+	var (
+		ackPkt  Ack
+		errPkt  Err
+		dataPkt = Data{Payload: bytes.NewReader(s.Payload)}
+		buf     = make([]byte, DatagramSize)
+	)
+
+NEXTPACKET:
+	for n := DatagramSize; n == DatagramSize; {
+		data, err := dataPkt.MarshalBinary()
+		if err != nil {
+			log.Printf("[%s] preparing data packet: %v", clientAddr, err)
+			return
+		}
+	RETRY:
+		for i := s.Retries; i > 0; i-- {
+			n, err = conn.Write(data)
+			if err != nil {
+				log.Printf("[%s] write: %v", clientAddr, err)
+				return
+			}
+
+			// wait for the client's ACK packet
+			_ = conn.SetReadDeadline(time.Now().Add(s.Timeout))
+
+			_, err = conn.Read(buf)
+			if err != nil {
+				if nErr, ok := err.(net.Error); ok && nErr.Timeout() {
+					continue RETRY
+				}
+
+				log.Printf("[%s] waiting  for ACK: %v", clientAddr, err)
+				return
+			}
+
+			switch {
+			case ackPkt.UnmarshalBinary(buf) == nil:
+				if uint16(ackPkt) == dataPkt.Block {
+					// received ACK; send next data packet
+					continue NEXTPACKET
+				}
+			case errPkt.UnmarshalBinary(buf) == nil:
+				log.Printf("[%s] received error: %v", clientAddr, errPkt.Message)
+				return
+			default:
+				log.Printf("[%s] bad papcket", clientAddr)
+			}
+
+		}
+		log.Printf("[%s] exhausted retries", clientAddr)
+		return
+	}
+	log.Printf("[%s] sent %d blocks", clientAddr, dataPkt.Block)
+}
--- ch6/types.go
+++ ch6/types.go
@@ -240,3 +240,25 @@ func (e Err) MarshalBinary() ([]byte, error) {
 	return b.Bytes(), nil
 
 }
+
+func (e *Err) UnmarshalBinary(p []byte) error {
+	r := bytes.NewBuffer(p)
+
+	var code OpCode
+	err := binary.Read(r, binary.BigEndian, &code) // read operation code
+	if err != nil {
+		return err
+	}
+	if code != OpErr {
+		return errors.New("invalid ERROR")
+	}
+
+	binary.Read(r, binary.BigEndian, &e.Error) // read error message
+	if err != nil {
+		return err
+	}
+
+	e.Message, err = r.ReadString(0)
+	e.Message = strings.TrimRight(e.Message, "\x00") // remove the 0-byte
+	return err
+}
