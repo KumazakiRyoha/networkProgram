487ddca3863fbd81b95e7936e0b26a64bb0b4aa2
rutaceae <1593713783@qq.com>
2023-09-08 23:59:25 +0900

    networkProgram day16

---
+++ ch6/tftp/tftp.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	tftp "networkProgram/ch6"
+)
+
+var (
+	address = flag.String("a", "127.0.0.1:69", "listen address")
+	payload = flag.String("p", "payload.svg", "file to serve to clients")
+)
+
+func main() {
+	flag.Parse()
+
+	p, err := ioutil.ReadFile(*payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := tftp.Server{Payload: p}
+	log.Fatal(s.ListenAndServer(*address))
+}
