7aab78ab9b511245621b8d8b423ed88d080b886e
rutaceae <1593713783@qq.com>
2023-09-14 23:56:28 +0900

    networkProgram day18

--- ch7/echo.go
+++ ch7/echo.go
@@ -18,7 +18,7 @@ func streamingEchoServer(ctx context.Context, network string,
 			_ = s.Close()
 		}()
 
-		for {
+		for { // 持续地等待客户端连接，并为每一个连接启动一个新的协程进行处理
 			conn, err := s.Accept()
 			if err != nil {
 				return
@@ -27,7 +27,7 @@ func streamingEchoServer(ctx context.Context, network string,
 			go func() {
 				defer func() { _ = conn.Close() }()
 
-				for {
+				for { //这个循环持续地从当前客户端读取数据，并将接收到的数据回显到客户端
 					buf := make([]byte, 1024)
 					n, err := conn.Read(buf)
 					if err != nil {
